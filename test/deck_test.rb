9e9b9d0022d4942137b7490fa4c5d53431e27135
Charles Kaminer <ckamine@g.clemson.edu>
2016-12-20 16:28:09 -0500

    create deck class deck class initialized with array of 52 cards create lookup constant hash to return numerical values all tests passing

---
+++ test/deck_test.rb
@@ -0,0 +1,28 @@
+require './lib/deck'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+
+class DeckTest < Minitest::Test
+  def test_deck_exists
+    d = Deck.new
+    assert_equal Deck, d.class
+  end
+
+  def test_deck_created_with_52_card_deck
+    d = Deck.new
+    assert_equal 52, d.cards.length
+  end
+
+  def test_there_are_thirteen_unique_card_values
+    d = Deck.new
+    assert_equal 13, d.cards.uniq.length
+  end
+
+  def test_numerical_value_lookup
+    d = Deck.new
+    assert_equal 2,  d.card_numerical_value("2")
+    assert_equal 10, d.card_numerical_value("K")
+    assert_equal 11, d.card_numerical_value("a")
+  end
+end
